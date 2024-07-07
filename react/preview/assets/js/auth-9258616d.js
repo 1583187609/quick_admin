a6dbfde6fead000ca31d81f530c7572fe3be5e27
fanlichuan <1583187609@qq.com>
2024-07-07 18:21:00 +0800

    feat: 更新

---
+++ react/preview/assets/js/auth-9258616d.js
@@ -0,0 +1,1 @@
+import{f as e}from"./index-d8656ef1.js";const o=t=>e("get","/auth/role/list",t),a=t=>e("get","/auth/menu/list",t);export{a as G,o as a};
--- react/preview/assets/js/mock-f2bf5329.js
+++
@@ -1,1 +0,0 @@
-import{f as m}from"./index-f8de5364.js";const s=o=>m("get","/mock/common/list",o),c=o=>m("post","/mock/common",o),n=o=>m("post","/mock/common/export",o);export{s as G,c as P,n as a};
